dbc70e71ba27bba8d9512a0982f9d960bb18e307
Haluk Cakan <halukcakan35@gmail.com>
2023-03-15 16:26:28 +0300

    3. komut

--- src/Day01Git.java
+++ src/Day01Git.java
@@ -2,5 +2,6 @@ public class Day01Git {
     public static void main(String[] args) {
         System.out.println("git 1. deneme");
         System.out.println("git 1 kusur deneme");
+        System.out.println("en son deneme");
     }
 }
