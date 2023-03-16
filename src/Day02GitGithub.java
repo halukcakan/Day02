9675ee5c3d03d124e8dc31fe5b8657487dd2994d
Haluk Cakan <halukcakan35@gmail.com>
2023-03-16 18:33:54 +0300

    V2

--- src/Day02GitGithub.java
+++ src/Day02GitGithub.java
@@ -1,5 +1,5 @@
 public class Day02GitGithub {
     public static void main(String[] args) {
-        System.out.println("GitHub'i da ogrendim");
+        System.out.println("Git'den GitHub'a push etmeyi ogrendim");
     }
 }
