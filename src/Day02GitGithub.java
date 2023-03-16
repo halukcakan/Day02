e10c3f19b74e3dd410da7771b09efee7248809e3
halukcakan <127951014+halukcakan@users.noreply.github.com>
2023-03-16 18:50:31 +0300

    Update Day02GitGithub.java

--- src/Day02GitGithub.java
+++ src/Day02GitGithub.java
@@ -1,5 +1,6 @@
 public class Day02GitGithub {
     public static void main(String[] args) {
         System.out.println("Git'den GitHub'a push etmeyi ogrendim");
+        System.out.println("GitHub'dan git'e pull yapmayi da ogrendim");
     }
 }
